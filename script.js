4adf0ff2e052088fe8457f98852d197cef917425
alexjustdoit225 <alex.minnie.aw@gmail.com>
2022-02-21 09:51:34 -0600

    Update script.js
    -added forEach loop to loop through gameObjects

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const c = canvas.getContext("2d");
 const CANVAS_WIDTH = canvas.width = 800;
 const CANVAS_HEIGHT = canvas.height = 700;
 //changes scrolling speed 
-let gameSpeed = 15;
+let gameSpeed = 10;
 
 const backgroundLayer1 = new Image();
 backgroundLayer1.src = "/assets/layer-1.png";
@@ -46,13 +46,21 @@ class Layer {
     }
 }
 
-const layer4 = new Layer(backgroundLayer4, 0.5); 
+const layer1 = new Layer(backgroundLayer1, 0.2); 
+const layer2 = new Layer(backgroundLayer2, 0.4); 
+const layer3 = new Layer(backgroundLayer3, 0.6); 
+const layer4 = new Layer(backgroundLayer4, 0.8); 
+const layer5 = new Layer(backgroundLayer5, 1); 
+
+const gameObjects = [layer1, layer2, layer3, layer4, layer5];
 
 function animate(){
     //clears canvas; deletes old animation frames
     c.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT); 
-    layer4.update(); 
-    layer4.draw();
+    gameObjects.forEach(object => {
+        object.update(); 
+        object.draw(); 
+    })
     requestAnimationFrame(animate);
 }; 
 animate(); 
