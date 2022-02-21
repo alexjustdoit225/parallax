4c7dd44120ddb1334333cc07030f9ff32d755cf8
alexjustdoit225 <alex.minnie.aw@gmail.com>
2022-02-21 09:40:33 -0600

    Update script.js

--- script.js
+++ script.js
@@ -17,8 +17,36 @@ const backgroundLayer5 = new Image();
 backgroundLayer5.src = "/assets/layer-5.png";
 
 class Layer {
-    
+    constructor(image, speedModifier){
+        this.x = 0; 
+        this.y =  0; 
+        this.width = 2400; 
+        this.height = 700; 
+        this.x2 = this.width; 
+        this.image = image; 
+        this.speedModifier = speedModifier; 
+        this.speed = gameSpeed * this.speedModifier; 
+    }
+    update(){
+        this.speed = gameSpeed * this.speedModifier;
+        if (this.x <= -this.width){
+            //makes sure there is no gap between images
+            this.x = this.width + this.x2 - this.speed; 
+        }
+        if (this.x2 <= -this.width){
+            //makes sure there is no gap between image
+            this.x2 = this.width + this.x - this.speed;
+        }
+        this.x = Math.floor(this.x - this.speed); 
+        this.x2 = Math.floor(this.x2 - this.speed); 
+    }
+    draw(){
+        c.drawImage(this.image, this.x, this.y, this.width, this.height);
+        c.drawImage(this.image, this.x2, this.y, this.width, this.height);
+    }
 }
+
+const layer4 = new Layer(backgroundLayer4, 0.5); 
 
 function animate(){
     //clears canvas; deletes old animation frames
